d11da491123a7c2009d7a85de1b6b4e0c959ceb6
Gustavo <gustavogomes1108@gmail.com>
2026-02-23 10:14:52 -0300

    .

---
+++ Aula 2/meu-primeiro-spring/src/main/java/com/example/meu_primeiro_spring/MeuPrimeiroSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.meu_primeiro_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroSpringApplication.class, args);
+	}
+
+}
---
+++ Aula 2/meu-primeiro-spring/src/test/java/com/example/meu_primeiro_spring/MeuPrimeiroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meu_primeiro_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
